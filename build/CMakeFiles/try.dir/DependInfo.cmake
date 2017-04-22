1e2033c88b11d81d44f3c610b988cfb34a09b3fc
Esref Ozturk <esrefozturk93@gmail.com>
2017-04-22 16:31:02 +0300

    cmake path changed to mac file path

--- build/CMakeFiles/try.dir/DependInfo.cmake
+++ build/CMakeFiles/try.dir/DependInfo.cmake
@@ -9,7 +9,7 @@ set(CMAKE_DEPENDS_CHECK_C
   "/opt/tools/bladerf-tun-interface/src/tryframing.c" "/opt/tools/bladerf-tun-interface/build/CMakeFiles/try.dir/src/tryframing.c.o"
   "/opt/tools/bladerf-tun-interface/src/utils.c" "/opt/tools/bladerf-tun-interface/build/CMakeFiles/try.dir/src/utils.c.o"
   )
-set(CMAKE_C_COMPILER_ID "GNU")
+set(CMAKE_C_COMPILER_ID "Clang")
 
 # The include file search paths:
 set(CMAKE_C_TARGET_INCLUDE_PATH
